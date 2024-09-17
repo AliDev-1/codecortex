093c1535afc0d76cc7d2895b5ae0e2851a21a845
Nabeel Ali <nabeelali.94@outlook.com>
2024-09-16 22:11:09 -0400

    Fix linting error for deployment 2

--- lib/actions/answer.actions.ts
+++ lib/actions/answer.actions.ts
@@ -39,7 +39,7 @@ export async function getAnswers(params: GetAnswersParams) {
   try {
     connectToDatabase();
 
-    const { questionId, sortBy, page = 1, pageSize = 10 } = params;
+    const { questionId, page = 1, pageSize = 10 } = params;
 
     const skipAmount = (page - 1) * pageSize;
 
